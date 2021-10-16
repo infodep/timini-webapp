b441434b52ac814fb423ac7f55ac545003f8d671
trondhauklien <36609741+trondhauklien@users.noreply.github.com>
2021-10-16 09:35:47 +0200

    New change of content

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import { Layout } from "../components/layout/Layout";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div>This is the home for timini.no</div>
+      <div>This is the new home for timini.no</div>
     </Layout>
   );
 };
