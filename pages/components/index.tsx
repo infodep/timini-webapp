ed1580e209a2904904d0312233a5f5f73edac187
Trond Hauklien <trond@hauklien.com>
2021-11-03 15:39:25 +0100

    Removed testing folder

--- pages/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import { Layout } from "../../components/layout/Layout";
-
-const Components: NextPage = () => {
-  return (
-    <Layout>
-      
-    </Layout>
-  )};
