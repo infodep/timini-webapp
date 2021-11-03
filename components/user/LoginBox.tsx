021f8ad7f87061cc421a7a3f7fe288e33ff54e7b
Trond Hauklien <trond@hauklien.com>
2021-11-03 15:38:54 +0100

    Temp  design using tailwindcss

--- components/user/LoginBox.tsx
+++ components/user/LoginBox.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import AuthContext from "../../contexts/auth";
 
+
 export const LoginBox = (): JSX.Element => {
   const [userData, setUserData] = useState({
     username: "",
@@ -29,30 +30,34 @@ export const LoginBox = (): JSX.Element => {
     }
   }
   return (
-    <div className="login">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
+    <div className="login flex-3">
+      <form onSubmit={handleSubmit} className="flex">
+        <div className="flex-1 flex mx-1">
+          {/*<label htmlFor="username">Username</label>*/}
 
-        <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Username"
-          value={userData.username}
-          onChange={(event) => setUserData(Object.assign({}, userData, { username: event.target.value }))}
-        />
-
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          value={userData.password}
-          onChange={(event) => setUserData(Object.assign({}, userData, { password: event.target.value }))}
-        />
-
-        <button type="submit">Login</button>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="p-2 border border-gray-300 outline-none focus:border-gray-400 text-black"
+            placeholder="Username"
+            value={userData.username}
+            onChange={(event) => setUserData(Object.assign({}, userData, { username: event.target.value }))}
+          />
+        </div>
+        <div className="flex-2 flex mx-1">
+          {/* <label htmlFor="password">Password</label> */}
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="p-2 border border-gray-300 outline-none focus:border-gray-400 text-black"
+            placeholder="Password"
+            value={userData.password}
+            onChange={(event) => setUserData(Object.assign({}, userData, { password: event.target.value }))}
+          />
+        </div>
+        <button className="flex-3 relative items-center justify-center bg-blue-400 p-2 ml-1 hover:bg-blue-800" type="submit">Login</button>
 
         {userData.error && <p className="error">Error: {userData.error}</p>}
       </form>
