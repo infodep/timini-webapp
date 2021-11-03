1a0412b45aad2b5bef8583edcb2d069341e63a87
iverks <iverks@stud.ntnu.no>
2021-11-03 23:53:57 +0100

    expose env variables to browser

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import type { AppProps } from "next/app";
 import axios from "axios";
 import { AuthProvider } from "../contexts/auth";
 
-axios.defaults.baseURL = process.env.BASE_URL || "http://localhost:8000";
+//Define NEXT_PUBLIC_BASE_URL in .env.local, next will automatically import
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8000";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
