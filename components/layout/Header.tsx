58ce438e9fab3c5c7764169ac1912fda041d5956
Trond Hauklien <trond@hauklien.com>
2021-11-03 15:37:13 +0100

    Temp navbar design with tailwindcss

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,25 +1,23 @@
-import { AppBar, Container } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
 import { UserOrLogin } from "../user/UserOrLogin";
+import logo from "../../assets/images/logo.svg";
 
 export const Header = (): JSX.Element => {
   return (
-    <AppBar className="header" position="relative">
-      <Container>
-        <Image src="/../../assets/images/logo.svg" alt="Timini" width="224px" height="75px" />
-        <div>
-          <UserOrLogin />
-        </div>
-        <nav>
+    <div className="w-screen bg-gray-800">
+      <div className="container mx-auto px-4">
+        <nav className=" text-white items-center flex">
+          <div className="my-2 mr-auto"><Image src={logo} alt="Timini" width={224} height={75}/></div>
           <Link href="/">
-            <a>Home</a>
+          <a><div className="relative flex-1 items-center justify-center bg-blue-400 p-2 mx-1 hover:bg-blue-800 ">Home</div></a>
           </Link>
           <Link href="/articles">
-            <a>Articles</a>
+            <a><div className="relative flex-2 items-center justify-center bg-blue-400 p-2 mx-1 hover:bg-blue-800">Articles</div></a>
           </Link>
+          <UserOrLogin />
         </nav>
-      </Container>
-    </AppBar>
+      </div>
+    </div>
   );
 };
